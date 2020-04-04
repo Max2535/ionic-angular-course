31547457b467f8a7111326c585e79b62c71714ce
suppchai <kalmro4@gmail.com>
2020-04-04 19:16:37 +0700

    commit

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,6 @@
+export interface Recipe{
+    id:string;
+    title:string;
+    imgaeUrl:string;
+    ingredients:string[];
+}
---
+++ src/app/recipes/recipes.page.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
+import { RecipesService } from './recipes.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.page.html',
+  styleUrls: ['./recipes.page.scss'],
+})
+export class RecipesPage implements OnInit {
+  recipes:Recipe[];
+  constructor(private recipesService:RecipesService,private route:ActivatedRoute) { 
+    route.params.forEach(params => {
+      //console.log(params);
+      this.recipes=this.recipesService.getAllRecipes();
+    });
+  }
+
+
+  ngOnInit() {
+    this.recipes=this.recipesService.getAllRecipes();
+  }
+
+}
