3e14ddd66540776249a65075db0afa533b72b7fc
suppchai <kalmro4@gmail.com>
2020-04-05 18:42:15 +0700

    Navigation & Routing in Ionic Apps

---
+++ src/app/places/places.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Place } from './place.mode';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlacesService {
+  private _places: Place[] = [
+    new Place('p1',
+      'Manhattan Mansion',
+      'In the heart of New York City.',
+      'https://www.amny.com/wp-content/uploads/2020/03/GettyImages-1089200736.jpg',
+      149.99),
+    new Place('p2',
+      'L\'Amour Toujours',
+      'A romantic place in Paris!',
+      'https://photos.mandarinoriental.com/is/image/MandarinOriental/paris-2017-home?$MO_masthead-property-mobile$',
+      189.99),
+    new Place('p3',
+      'The Foggy Palace',
+      'Not your average city trip!',
+      'https://cache.gmo2.sistacafe.com/images/uploads/summary/image/35969/City_Trip_Rome.jpg',
+      99.99)
+  ];
+
+  get places() {
+    return [...this._places];
+  }
+  constructor() { 
+  }
+
+  getPlace(id:string){
+    return {...this._places.find(
+      p=>p.id===id)};  
+  }
+
+}
--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { RecipesService } from './recipes.service';
 import { ActivatedRoute } from '@angular/router';
@@ -8,18 +8,41 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './recipes.page.html',
   styleUrls: ['./recipes.page.scss'],
 })
-export class RecipesPage implements OnInit {
+export class RecipesPage implements OnInit,OnDestroy {
   recipes:Recipe[];
   constructor(private recipesService:RecipesService,private route:ActivatedRoute) { 
-    route.params.forEach(params => {
+    /*route.params.forEach(params => {
       //console.log(params);
       this.recipes=this.recipesService.getAllRecipes();
-    });
+    });*/
   }
 
 
-  ngOnInit() {
+  ngOnInit() {    
+    console.log('ngOnInit');
+    console.log(this.recipes);
+  }
+
+  ionViewWillEnter(){
     this.recipes=this.recipesService.getAllRecipes();
+    console.log('ionViewWillEnter');
+  }
+
+  ionViewDidEnter(){
+    console.log('ionViewDidEnter');
+  }
+
+  ionViewWillLeave(){
+    console.log('ionViewWillLeave');
+  }
+
+  ionViewDidLeave(){
+    console.log('ionViewDidLeave');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+    
   }
 
 }
